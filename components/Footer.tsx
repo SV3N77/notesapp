a8fab6b7438221627c0ebd859013d51693693faf
SV3N77 <danny.jiang1234@hotmail.com>
2022-11-24 11:32:42 +1100

    initial project set up

---
+++ components/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div className="flex bg-lime-200 p-10"></div>;
+}
---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex bg-lime-200 p-10">
+      <h1 className="">Notes App</h1>
+    </div>
+  );
+}
