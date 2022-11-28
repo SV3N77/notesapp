2a20bfa792986e92235cabbc3ec8def33fd72445
SV3N77 <danny.jiang1234@hotmail.com>
2022-11-29 10:27:27 +1100

    add login form

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <div className="flex bg-lime-200 p-10">
-      <h1 className="">Notes App</h1>
+    <div className="flex bg-yellow-200 p-10 text-neutral-600">
+      <h1 className="font-sans text-2xl font-bold">Notes App</h1>
     </div>
   );
 }
